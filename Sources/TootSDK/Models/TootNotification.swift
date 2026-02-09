18cc78b5e9b43775037d33dfaf5191246ad816dd
Dominic Williams <dom@pixelfolio.app>
2026-02-09 08:44:35 -0400

    feat: Add moderationWarning notification type

--- Sources/TootSDK/Models/TootNotification.swift
+++ Sources/TootSDK/Models/TootNotification.swift
@@ -71,6 +71,8 @@ public struct TootNotification: Codable, Hashable, Identifiable, Sendable {
         case emojiReaction
         /// Annual report is available
         case annualReport
+        /// Moderation warning received
+        case moderationWarning
 
         /// An unsupported notification type was received. If you encounter this please update TootSDK by adding support for received type.
         case unknown(String)
@@ -120,6 +122,8 @@ public struct TootNotification: Codable, Hashable, Identifiable, Sendable {
                 self = .emojiReaction
             case "annual_report":
                 self = .annualReport
+            case "moderation_warning":
+                self = .moderationWarning
             default:
                 self = .unknown(rawValue)
             }
@@ -142,6 +146,7 @@ public struct TootNotification: Codable, Hashable, Identifiable, Sendable {
             case .severedRelationships: return "severed_relationships"
             case .emojiReaction: return "emoji_reaction"
             case .annualReport: return "annual_report"
+            case .moderationWarning: return "moderation_warning"
             case .unknown(let rawValue): return rawValue
             }
         }
@@ -177,6 +182,7 @@ public struct TootNotification: Codable, Hashable, Identifiable, Sendable {
                 .severedRelationships,
                 .emojiReaction,
                 .annualReport,
+                .moderationWarning,
             ]
         }
 
@@ -186,7 +192,7 @@ public struct TootNotification: Codable, Hashable, Identifiable, Sendable {
             case .mastodon:
                 return [
                     .follow, .mention, .repost, .favourite, .poll, .followRequest, .post, .update, .quote, .quotedUpdate,
-                    .adminSignUp, .adminReport, .severedRelationships, .annualReport,
+                    .adminSignUp, .adminReport, .severedRelationships, .annualReport, .moderationWarning,
                 ]
             case .pleroma, .akkoma:
                 return [.follow, .mention, .repost, .favourite, .poll, .followRequest, .update, .emojiReaction]
